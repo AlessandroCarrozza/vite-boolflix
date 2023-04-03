953ab9b050df9f93cecedeeb93f5cd09dcb5e3c3
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-04-03 17:26:39 +0200

    Aggiunta del title "serie"

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -27,6 +27,7 @@ export default {
             </FilmCard>
         </div>
 
+        <h2 v-if="store.seriesList.length > 0">Serie</h2>
         <div class="wrapper series">
             <SerieCard v-for="(serie, index) in store.seriesList" :key="index" :title="serie.name"
                 :originalTitle="serie.original_name" :language="serie.original_language" :average="serie.vote_average / 2"
