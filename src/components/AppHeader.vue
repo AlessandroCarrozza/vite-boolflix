956ed1d759417684560424aa39a28481c5f4495d
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-04-04 12:14:43 +0200

    Aggiunta di variables e mixins

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,10 +12,12 @@ export default {
 }
 </script>
 
+
+
 <template>
     <header>
         <div class="container">
-            <h1 class="logo-name">BOOLFLIX</h1>
+            <h1 class="logo-name"><a href="#">BOOLFLIX</a></h1>
 
             <div class="input-search">
                 <input type="text" placeholder="Cerca un film o una serie" v-model="store.searchText"
@@ -26,28 +28,55 @@ export default {
     </header>
 </template>
 
+
+
 <style scoped lang="scss">
 @use "../styles/general.scss";
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
 
-.input-search {
-    width: 250px;
-    height: 35px;
-    display: flex;
+header {
+    height: 80px;
+    width: 100%;
+    background-color: $blk-color;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
 
-    input {
-        width: 180px;
-        padding-left: 0.5rem;
-        outline: none;
+    .container {
+        @include flex-ali-center;
+        justify-content: space-between;
+        height: 100%;
+        max-width: 1600px;
+        margin: auto;
+        padding: 0 2rem;
+
+        .logo-name a {
+            color: $primary-color;
+            text-decoration: none;
+        }
+
+        .input-search {
+            width: 250px;
+            height: 35px;
+            display: flex;
+
+            input {
+                width: 180px;
+                padding-left: 0.5rem;
+                outline: none;
+            }
+
+            .btn-search {
+                width: 70px;
+                background-color: red;
+                @include flex-center;
+                color: $wht-color;
+                cursor: pointer;
+            }
+        }
     }
 
-    .btn-search {
-        width: 70px;
-        background-color: red;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: white;
-        cursor: pointer;
-    }
 }
 </style>
